12c724e168dbee1437592331718334f6f2539246
primalrambler <primal.rambler@gmail.com>
2016-09-16 16:30:34 -0500

    changed it so it had one insert command for multiple entries

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,17 +1,10 @@
 USE codeup_test_db;
 INSERT INTO albums (artist, name, release_date, genre, sales)
-VALUES ('Michael Jackson','Thriller', 1982, 'Pop, rock, R&B', 46);
-INSERT INTO albums (artist, name, release_date, genre, sales)
-VALUES ('AC/DC', 'Back in Black',1980,'Hard rock', 26.1);
-INSERT INTO albums (artist, name, release_date, genre, sales)
-VALUES ('Pink Floyd', 'The Dark Side of the Moon', 1973, 'Progressive rock', 24.2);
-INSERT INTO albums (artist, name, release_date, genre, sales)
-VALUES ('Whitney Houston', 'The Bodyguard', 1992, 'Soundtrack, R&B, soul, pop', 27.4);
-INSERT INTO albums (artist, name, release_date, genre, sales)
-VALUES ('Meat Loaf', 'Bat Out of Hell', 1977, 'Hard rock, progressive rock', 20.6);
-INSERT INTO albums (artist, name, release_date, genre, sales)
-VALUES ('Eagles', 'Their Greatest Hits (1971–1975)', 1976, 'Rock, soft rock, folk rock', 32.2);
-INSERT INTO albums (artist, name, release_date, genre, sales)
-VALUES ('Bee Gees', 'Saturday Night Fever', 1977, 'Disco', 20.6);
-INSERT INTO albums (artist, name, release_date, genre, sales)
-VALUES ('Fleetwood Mac', 'Rumours', 1977, 'Soft rock', 27.9);
+VALUES ('Michael Jackson','Thriller', 1982, 'Pop, rock, R&B', 46),
+	('AC/DC', 'Back in Black',1980,'Hard rock', 26.1),
+	('Pink Floyd', 'The Dark Side of the Moon', 1973, 'Progressive rock', 24.2),
+	('Whitney Houston', 'The Bodyguard', 1992, 'Soundtrack, R&B, soul, pop', 27.4),
+	('Meat Loaf', 'Bat Out of Hell', 1977, 'Hard rock, progressive rock', 20.6),
+	('Eagles', 'Their Greatest Hits (1971–1975)', 1976, 'Rock, soft rock, folk rock', 32.2),
+	('Bee Gees', 'Saturday Night Fever', 1977, 'Disco', 20.6),
+	('Fleetwood Mac', 'Rumours', 1977, 'Soft rock', 27.9);
