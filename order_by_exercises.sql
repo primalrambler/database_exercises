6393eb2c2d8326e2462a500b8d3f265bb041dfac
primalrambler <primal.rambler@gmail.com>
2016-09-20 10:28:58 -0500

    sorting E names by employee number. no change b/c that's the primary key

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -4,10 +4,11 @@ first_name = 'Irena'
 OR first_name ='Vidya' 
 OR first_name = 'Maya'
 )
-ORDER BY first_name, last_name;
+ORDER BY last_name, first_name;
 
 -- begins with an E or ends with an E
-SELECT * FROM employees WHERE last_name LIKE '%e' OR last_name LIKE 'e%';
+SELECT * FROM employees WHERE last_name LIKE '%e' OR last_name LIKE 'e%'
+ORDER BY emp_no;
 
 -- begins and ends with an E
 SELECT * FROM employees WHERE last_name LIKE '%e' AND last_name LIKE 'e%';
