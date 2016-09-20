659b0ed716b005c9487ead8ff345acaa89a143c5
primalrambler <primal.rambler@gmail.com>
2016-09-20 09:29:06 -0500

    filtered an OR chain for males only

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,4 +1,10 @@
-SELECT * FROM employees WHERE first_name IN ('Irena','Vidya','Maya');
+SELECT * FROM employees WHERE gender = 'M' AND (
+first_name = 'Irena' 
+OR first_name ='Vidya' 
+OR first_name = 'Maya'
+);
+
+SELECT * FROM employees WHERE first_name = 'Irena'
 
 SELECT * FROM employees WHERE last_name LIKE 'E%';
 
