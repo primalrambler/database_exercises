3feb9862d71bb8a74e479c1de7bf2625dabcdbec
primalrambler <primal.rambler@gmail.com>
2016-09-20 09:36:51 -0500

    added query to find all christmas birthday folks hired in the 90's

--- where_exercises.sql
+++ where_exercises.sql
@@ -7,3 +7,5 @@ OR first_name = 'Maya'
 SELECT * FROM employees WHERE last_name LIKE '%e' OR last_name LIKE 'e%';
 
 SELECT * FROM employees WHERE last_name LIKE '%e' AND last_name LIKE 'e%';
+
+SELECT * FROM employees WHERE birth_date LIKE '%-12-25' AND hire_date LIKE '199%';
