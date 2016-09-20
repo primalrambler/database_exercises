6d09d5f9fe72d9a7b90fbf5aedd5200cb8b61da3
primalrambler <primal.rambler@gmail.com>
2016-09-20 09:33:32 -0500

    created query to find names beginning or ending with a letter. created query to find names beginning and ending with a letter.

--- where_exercises.sql
+++ where_exercises.sql
@@ -4,12 +4,6 @@ OR first_name ='Vidya'
 OR first_name = 'Maya'
 );
 
-SELECT * FROM employees WHERE first_name = 'Irena'
+SELECT * FROM employees WHERE last_name LIKE '%e' OR last_name LIKE 'e%';
 
-SELECT * FROM employees WHERE last_name LIKE 'E%';
-
-SELECT * FROM employees WHERE hire_date LIKE '199%';
-
-SELECT * FROM employees WHERE birth_date LIKE '%-12-25';
-
-SELECT * FROM employees WHERE last_name LIKE '%Q%';
+SELECT * FROM employees WHERE last_name LIKE '%e' AND last_name LIKE 'e%';
