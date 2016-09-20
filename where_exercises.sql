ebee877e0c8b37a79ab34337efa74132ffa2dc04
primalrambler <primal.rambler@gmail.com>
2016-09-20 09:39:15 -0500

    query to find names with Q but not QU

--- where_exercises.sql
+++ where_exercises.sql
@@ -9,3 +9,5 @@ SELECT * FROM employees WHERE last_name LIKE '%e' OR last_name LIKE 'e%';
 SELECT * FROM employees WHERE last_name LIKE '%e' AND last_name LIKE 'e%';
 
 SELECT * FROM employees WHERE birth_date LIKE '%-12-25' AND hire_date LIKE '199%';
+
+SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
