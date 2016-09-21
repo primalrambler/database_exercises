030d5064ad8f8b3680068e68451802e47ba2b65b
primalrambler <primal.rambler@gmail.com>
2016-09-21 16:49:14 -0500

    changed indentation added a join to the employee table to remove problem with joining two other tables with 2 foreign keys

--- join_exercises.sql
+++ join_exercises.sql
@@ -25,14 +25,12 @@ LEFT JOIN users
 ON roles.id = users.role_id
 GROUP BY roles.name;
 
-
-
-
 -- shows each department along with the name of the current manager for that department
 
-SELECT d.dept_name AS 'Department', concat(e.first_name,' ',e.last_name) AS 'Department Manager' FROM dept_manager AS dm
-JOIN departments AS d
-ON dm.dept_no = d.dept_no
+SELECT d.dept_name AS 'Department', concat(e.first_name,' ',e.last_name) AS 'Department Manager' 
+FROM departments AS d
+JOIN dept_manager AS dm
+	ON d.dept_no = dm.dept_no
 JOIN employees AS e
 ON dm.emp_no = e.emp_no
 WHERE dm.to_date = '9999-01-01';
@@ -41,40 +39,50 @@ WHERE dm.to_date = '9999-01-01';
 
 SELECT d.dept_name AS 'Department', concat(e.first_name,' ',e.last_name) AS 'Department Manager' FROM dept_manager AS dm
 JOIN departments AS d
-ON dm.dept_no = d.dept_no
+	ON dm.dept_no = d.dept_no
 JOIN employees AS e
-ON dm.emp_no = e.emp_no
-WHERE dm.to_date = '9999-01-01' AND e.gender = 'F';
+	ON dm.emp_no = e.emp_no
+WHERE dm.to_date = '9999-01-01' 
+	AND e.gender = 'F';
 
 -- Find the current titles of employees currently working in the Customer Service department.
 
-SELECT t.title AS 'Title', count(t.title) AS 'Count' FROM dept_emp AS de
+SELECT t.title AS 'Title', count(*) AS 'Count'
+FROM employees AS e
+JOIN dept_emp AS de
+	ON e.emp_no = de.emp_no
 JOIN titles AS t
-ON de.emp_no = t.emp_no
-WHERE de.dept_no = 'd009' AND t.to_date = '9999-01-01'
+	ON e.emp_no = t.emp_no
+WHERE de.dept_no = 'd009'
+	AND de.to_date = '9999-01-01'
+	AND t.to_date = '9999-01-01'
 GROUP BY t.title;
 
 -- Find the current salary of all current managers.
 SELECT d.dept_name AS 'Department', CONCAT(e.first_name,' ', e.last_name) AS 'Manager', s.salary AS 'Salary' FROM dept_manager AS dm
 JOIN departments AS d
-ON dm.dept_no = d.dept_no
+	ON dm.dept_no = d.dept_no
 JOIN employees AS e
-ON dm.emp_no = e.emp_no
+	ON dm.emp_no = e.emp_no
 JOIN salaries AS s
-ON e.emp_no = s.emp_no
-WHERE s.to_date = '9999-01-01' AND dm.to_date = '9999-01-01'
+	ON e.emp_no = s.emp_no
+WHERE s.to_date = '9999-01-01' 
+	AND dm.to_date = '9999-01-01'
 GROUP BY d.dept_name;
 
 -- Bonus Find the names of all current employees, their department name, and their current manager's NAME
 
-SELECT CONCAT(e.first_name,' ',e.last_name) AS 'Employee', d.dept_name AS 'Department', CONCAT(emgr.first_name,' ',emgr.last_name) AS 'Manager'
+SELECT CONCAT(e.first_name,' ',e.last_name) AS 'Employee', d.dept_name AS 'Department', CONCAT(dme.first_name,' ',dme.last_name) AS 'Manager'
 FROM employees AS e
 JOIN dept_emp AS de
-ON e.emp_no = de.emp_no
+	ON e.emp_no = de.emp_no
 JOIN departments AS d
-ON de.dept_no = d.dept_no
+	ON de.dept_no = d.dept_no
 JOIN dept_manager AS dm
-ON d.dept_no = dm.dept_no
-JOIN employees AS emgr
-ON dm.emp_no = emgr.emp_no
-WHERE de.to_date = '9999-01-01' AND dm.to_date = '9999-01-01';
+	ON d.dept_no = dm.dept_no
+JOIN employees AS dme
+	ON dm.emp_no = dme.emp_no
+WHERE de.to_date = '9999-01-01' 
+	AND dm.to_date = '9999-01-01';
+	
+	
